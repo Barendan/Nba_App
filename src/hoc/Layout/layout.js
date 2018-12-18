884f0e74f310cc47c42cf1eac7c1546702f4341a
Daniel Barenboim <barendan@gmail.com>
2018-12-18 16:51:25 -0500

    added hoc that will contain the navigation and sidenav

--- src/hoc/Layout/layout.js
+++ src/hoc/Layout/layout.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
-
+import Header from '../../components/Header/header';
+import './layout.css';
 
 class Layout extends Component {
 
 	state = {
+		showNav:false
+	}
 
+	toggleSideNav = (action) => {
+		this.setState({
+			showNav:action
+		})
 	}
 
 	render() {
 		return(
 			<div>
-				Header
+				<Header
+					showNav={this.state.showNav}
+					onHideNav={() => this.toggleSideNav(false)}
+					onOpenNav={() => this.toggleSideNav(true)}
+				/>
 				{this.props.children}
 				Footer
 			</div>
